5f51f5d59f5535dd71e738c3d4ada208dfa52dc5
EElBard <101231052+EElBard@users.noreply.github.com>
2023-08-19 21:33:48 -0700

    Add intro text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
 
 const Home = () => {
 
@@ -6,8 +8,14 @@ const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br /> I'm</h1>          
+        <h1>Hi, <br /> I'm
         <img src={LogoTitle} alt="developer" />
+        lobodan
+        <br />
+        web developer
+        </h1>
+        <h2>Frontend Developer / Lighting Designer / Video Editor</h2>
+        <Link to="\contact" className='flat-button'>CONTACT ME</Link>
       </div>
     </div>
   )
