cfe01d48d7c7a01894fd8ac196613ef0bec842be
EElBard <ethan.elbard@gmail.com>
2023-08-19 17:25:19 -0700

    Added LinkedIn link

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 
 const Sidebar = () => (
@@ -23,6 +24,13 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/ethan-el-bard-407a4b22b">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
