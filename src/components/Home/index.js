e12865e70eafd047c1506bfeb929ae0fbea14a0f
EElBard <101231052+EElBard@users.noreply.github.com>
2023-08-22 21:59:21 -0700

    Added home page letter animation
    43:50

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,45 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-e.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import { useInsertionEffect, useState } from 'react';
+import Logo from './Logo';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = "than".split('')
+  const jobArray = "web developer.".split('')
 
+  useInsertionEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br /> I'm
-        <img src={LogoTitle} alt="developer" />
-        lobodan
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i</span>
+          <span className={`${letterClass} _13`}>,</span>
+          <br />
+          <span className={`${letterClass} _14`}>I</span>
+          <span className={`${letterClass} _15`}>'</span>
+          <span className={`${letterClass} _16`}>m</span>
+        <img src={LogoTitle} alt="e" />
+        <AnimatedLetters letterClass={letterClass}
+        strArray={nameArray}
+        idx={17} />
         <br />
-        web developer
+        <AnimatedLetters letterClass={letterClass}
+        strArray={jobArray}
+        idx={21} />
         </h1>
         <h2>Frontend Developer / Lighting Designer / Video Editor</h2>
-        <Link to="\contact" className='flat-button'>CONTACT ME</Link>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
       </div>
+      <Logo />
     </div>
   )
 }
