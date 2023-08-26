dc22f9abc2389ecc03789586358685d241d1fa2d
EElBard <ethan.elbard@gmail.com>
2023-08-26 15:01:50 -0700

    Added button to About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,11 @@
 import './index.scss';
 
 const About = () => {
-
+    return (
+        <div className='section-select'>
+            <input type='button' className='selection-button' value='Lighting'></input>
+        </div>
+    )
 }
 
 export default About;
