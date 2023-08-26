1afa47cf6204558821d11d30667de77aaff8a4a6
EElBard <101231052+EElBard@users.noreply.github.com>
2023-08-26 16:31:33 -0700

    Added button styles to About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,8 +2,12 @@ import './index.scss';
 
 const About = () => {
     return (
-        <div className='section-select'>
-            <input type='button' className='selection-button' value='Lighting'></input>
+        <div className='about-page'>
+            <div className='flex-container'>
+                <input type='button' className='selection-button' value='Lighting' title='Lighting'></input>
+                <input type='button' className='selection-button' value='Web Development' title='Web Development'></input>
+                <input type='button' className='selection-button' value='Video Editing' title='Video Editing'></input>
+            </div>
         </div>
     )
 }
