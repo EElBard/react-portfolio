87d37e0bd711adda8b285355b612fa7f66db0c65
EElBard <101231052+EElBard@users.noreply.github.com>
2023-08-26 23:20:07 -0700

    Added Flexbox framework to About Page

---
+++ src/components/About/Event Lighting/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const EventLighting = () => {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default EventLighting
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,15 @@
+import EventLighting from './Event Lighting';
 import './index.scss';
 
 const About = () => {
     return (
         <div className='about-page'>
             <div className='flex-container'>
-                <input type='button' className='selection-button' value='Lighting' title='Lighting'></input>
-                <input type='button' className='selection-button' value='Web Development' title='Web Development'></input>
-                <input type='button' className='selection-button' value='Video Editing' title='Video Editing'></input>
+                <input type='button' activeclassname='active' className='selection-button' value='Event Lighting' title='Event Lighting'></input>
+                <input type='button' activeclassname='active' className='selection-button' value='Web Development' title='Web Development'></input>
+                <input type='button' activeclassname='active' className='selection-button' value='Video Editing' title='Video Editing'></input>
             </div>
+            <EventLighting />
         </div>
     )
 }
