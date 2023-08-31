cbcf8aa3aa14ecd1536ecacb12578d78bdf0f508
EElBard <101231052+EElBard@users.noreply.github.com>
2023-08-31 03:41:32 -0700

    Added active button handling for about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,42 @@
+import { useState } from 'react';
 import EventLighting from './Event Lighting';
 import './index.scss';
 
 const About = () => {
-    return (
-        <div className='about-page'>
-            <div className='section-select flex-container'>
-                <input type='button' activeclassname='active' className='selection-button' value='Event Lighting' title='Event Lighting'></input>
-                <input type='button' activeclassname='active' className='selection-button' value='Web Development' title='Web Development'></input>
-                <input type='button' activeclassname='active' className='selection-button' value='Video Editing' title='Video Editing'></input>
-            </div>
-            <EventLighting />
-        </div>
-    )
-}
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (value) => {
+    setActiveButton(value);
+  };
+
+  return (
+    <div className='about-page'>
+      <div className='section-select flex-container'>
+        <input
+          type='button'
+          className={`selection-button ${activeButton === 'Event Lighting' ? 'active' : ''}`}
+          value='Event Lighting'
+          title='Event Lighting'
+          onClick={() => handleButtonClick('Event Lighting')}
+        />
+        <input
+          type='button'
+          className={`selection-button ${activeButton === 'Web Development' ? 'active' : ''}`}
+          value='Web Development'
+          title='Web Development'
+          onClick={() => handleButtonClick('Web Development')}
+        />
+        <input
+          type='button'
+          className={`selection-button ${activeButton === 'Video Editing' ? 'active' : ''}`}
+          value='Video Editing'
+          title='Video Editing'
+          onClick={() => handleButtonClick('Video Editing')}
+        />
+      </div>
+      <EventLighting />
+    </div>
+  );
+};
 
 export default About;
