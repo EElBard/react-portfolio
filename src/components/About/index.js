413eceb6689119f00382ab922e34aa7ead00422e
EElBard <ethan.elbard@gmail.com>
2023-10-11 19:03:20 -0700

    Added conditional component loading

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,45 +1,54 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import EventLighting from './Event Lighting';
+//import WebDevelopment from './Web Development'; // Import other components as needed
+//import VideoEditing from './Video Editing'; // Import other components as needed
 import './index.scss';
 import Loader from 'react-loaders';
 
 const About = () => {
   const [activeButton, setActiveButton] = useState(null);
 
+  const buttons = [
+    { label: 'Event Lighting', value: 'Event Lighting' },
+    { label: 'Web Development', value: 'Web Development' },
+    { label: 'Video Editing', value: 'Video Editing' },
+  ];
+
   const handleButtonClick = (value) => {
     setActiveButton(value);
   };
 
+  // Render different components based on the selected button
+  const renderSelectedComponent = () => {
+    switch (activeButton) {
+      case 'Event Lighting':
+        return <EventLighting />;
+      /*case 'Web Development':
+        return <WebDevelopment />;
+      case 'Video Editing':
+        return <VideoEditing />;*/
+      default:
+        return null;
+    }
+  };
+
   return (
-    <>
-    <div className='about-page'>
-      <div className='section-select flex-container'>
-        <input
-          type='button'
-          className={`selection-button ${activeButton === 'Event Lighting' ? 'active' : ''}`}
-          value='Event Lighting'
-          title='Event Lighting'
-          onClick={() => handleButtonClick('Event Lighting')}
-        />
-        <input
-          type='button'
-          className={`selection-button ${activeButton === 'Web Development' ? 'active' : ''}`}
-          value='Web Development'
-          title='Web Development'
-          onClick={() => handleButtonClick('Web Development')}
-        />
-        <input
-          type='button'
-          className={`selection-button ${activeButton === 'Video Editing' ? 'active' : ''}`}
-          value='Video Editing'
-          title='Video Editing'
-          onClick={() => handleButtonClick('Video Editing')}
-        />
+    <div className="about-page">
+      <div className="section-select flex-container">
+        {buttons.map((button) => (
+          <input
+            key={button.value}
+            type="button"
+            className={`selection-button ${activeButton === button.value ? 'active' : ''}`}
+            value={button.label}
+            title={button.label}
+            onClick={() => handleButtonClick(button.value)}
+          />
+        ))}
       </div>
-      <EventLighting />
+      {renderSelectedComponent()}
+      <Loader type="pacman" />
     </div>
-    <Loader type='pacman' />
-    </>
   );
 };
 
