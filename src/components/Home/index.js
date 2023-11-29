ef6bff76b162e2630b50117333cc3888ed367208
EElBard <ethan.elbard@gmail.com>
2023-11-29 11:04:35 -0800

    Changed contact to mailto, added react-helmet

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,7 @@ import Loader from 'react-loaders';
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const nameArray = "than,".split('')
-  const jobArray = "Full-Stack Developer.".split('')
+  const jobArray = "Event Lighting Guy.".split('')
 
   useInsertionEffect(() => {
     setTimeout(() => {
@@ -37,8 +37,8 @@ const Home = () => {
         strArray={jobArray}
         idx={21} />
         </h1>
-        <h2>Full-Stack Developer / Lighting Designer</h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <h2>Lighting Designer / Lightboard Programmer</h2>
+        <Link to="mailto:ethan.elbard@gmail.com" className='flat-button'>CONTACT ME</Link>
       </div>
       <Logo />
     </div>
